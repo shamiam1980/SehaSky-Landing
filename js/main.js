ec741bed8ecaac03ba1f546a89beb17465869569
shamiam <el.erm.el.erm@gmail.com>
2020-03-25 14:31:28 +0200

    Added animated logo / social media links / corrected anchors

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// var bm = document.querySelector('#bm');
+
+// var myAnimation = bodymovin.loadAnimation({
+//     container: bm,
+//     renderer: 'svg',
+//     loop: true,
+//     autoplay: true,
+//     path: '../img/data.json'
+// });
+
+// bm.addEventListener('mouseenter', () => {
+//     console.log('Is that REAL?');
+//     myAnimation.setDirection(1)
+//     myAnimation.play();
+// });
+
+// bm.addEventListener('mouseleave', () => {
+//     console.log('NO It is NOT!');
+//     myAnimation.setDirection(-1)
+//     myAnimation.play();
+// });
